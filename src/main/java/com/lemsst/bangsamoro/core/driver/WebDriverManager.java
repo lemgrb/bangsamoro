57dda2f4e34a3181024267b821c234f4b74957e4
Lemuel Bulangis <lemgrb@gmail.com>
2019-02-24 22:38:18 +0800

    Hotfix: Maximize window

--- src/main/java/com/lemsst/bangsamoro/core/driver/WebDriverManager.java
+++ src/main/java/com/lemsst/bangsamoro/core/driver/WebDriverManager.java
@@ -16,8 +16,10 @@ public abstract class WebDriverManager {
     }
 
     public WebDriver getDriver() {
-        if(driver==null)
+        if(driver==null) {
             create();
+            driver.manage().window().maximize();
+        }
         return driver;
     }
 
